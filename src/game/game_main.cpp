fb4522168d7bbd3c68b1f678b4663717fdf25c33
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-01-31 21:21:58 +0100

    Add game states

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -7,17 +7,66 @@ namespace Game {
 
 void entity_registration() {}
 
+enum class GameState {
+    INTRO,
+    GAME,
+    TRANSITION,
+    THERAPY
+};
+
+GameState game_state = GameState::GAME;
+
 void setup() {
     Renderer::turn_on_camera(0);
 }
 
 // Main logic
-void update(f32 delta) {}
+void update(f32 delta) {
+
+    // Determine which part of the game we are in.
+    switch (game_state) {
+
+        // Update intro.
+        case GameState::INTRO:
+            break;
+
+        // Update game.
+        case GameState::GAME:
+            break;
+
+        // Update transition.
+        case GameState::TRANSITION:
+            break;
+
+        // Update thrapy session.
+        case GameState::THERAPY:
+            break;
+    }
+}
 
 // Main draw
 void draw() {
     const char *some_string = "SeaCow";
-    Renderer::draw_text(some_string, 0, 0, 1.0, ASSET_MONACO_FONT, -0.5);
+
+    // Determine which part of the game we are in.
+    switch (game_state) {
+
+        // Draw intro.
+        case GameState::INTRO:
+            break;
+
+        // Draw game.
+        case GameState::GAME:
+            break;
+
+        // Draw transition.
+        case GameState::TRANSITION:
+            break;
+
+        // Draw thrapy session.
+        case GameState::THERAPY:
+            break;
+    }
 }
 
 }  // namespace Game
