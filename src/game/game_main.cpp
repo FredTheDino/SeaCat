1df16f53bcb27859886ad00a43f65cc802e8c169
Gustav Sörnäs <gusso230@student.liu.se>
2020-01-31 22:26:28 +0100

    First test phase2 movement

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -9,15 +9,51 @@ void entity_registration() {}
 
 void setup() {
     Renderer::turn_on_camera(0);
+
+    Renderer::get_camera(0)->zoom = 0.1;
+
+    using namespace Input;
+    add(K(a), Name::LEFT);
+    add(K(d), Name::RIGHT);
+    add(K(w), Name::UP);
+    add(K(s), Name::DOWN);
 }
 
+// scroll
+const f32 MAX_SPEED = 10;  // ship
+f32 vel_rel = 0.4;
+f32 floatiness = 4;
+Vec2 vel = V2(0, 0);  // change per frame
+Vec2 pos = V2(0, 0);  // current position
+const Vec2 DIM = V2(1, 1);  // size of rect
+
 // Main logic
-void update(f32 delta) {}
+void update(f32 delta) {
+    static bool show_control_controls = true;
+    if (Util::begin_tweak_section("controls", &show_control_controls)) {
+        Util::tweak("floatiness", &floatiness);
+        Util::tweak("vel_rel", &vel_rel);
+    }
+    Util::end_tweak_section(&show_control_controls);
+
+    using namespace Input;
+    Vec2 vel_target = V2(0, 0);
+    if (down(Name::UP))
+        vel_target.y += vel_rel;
+    if (down(Name::DOWN))
+        vel_target.y -= vel_rel;
+    if (down(Name::LEFT))
+        vel_target.x -= vel_rel;
+    if (down(Name::RIGHT))
+        vel_target.x += vel_rel;
+
+    vel += (vel_target - vel) * floatiness * delta;
+    pos += vel;
+}
 
 // Main draw
 void draw() {
-    const char *some_string = "SeaCow";
-    Renderer::draw_text(some_string, 0, 0, 1.0, ASSET_MONACO_FONT, -0.5);
+    Renderer::push_rectangle(0, pos, DIM);
 }
 
 }  // namespace Game
