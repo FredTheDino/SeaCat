317281dd6d10000f252c6701d20ee554b7f073dc
vollells <vicle728@student.liu.se>
2020-01-31 20:57:13 +0100

    New Branch For The Liberation Of Code from the Capitalist State

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -18,6 +18,8 @@ void update(f32 delta) {}
 void draw() {
     const char *some_string = "SeaCow";
     Renderer::draw_text(some_string, 0, 0, 1.0, ASSET_MONACO_FONT, -0.5);
+    const char *new_string = "Hello";
+    Renderer::draw_text(new_string, 0, 0, 1.0, ASSET_MONACO_FONT, -0.5);
 }
 
 }  // namespace Game
