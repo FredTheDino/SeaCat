512a6685f953a2b292f889f472801aa7184af737
vollells <vicle728@student.liu.se>
2020-02-01 00:52:40 +0100

    Segfaults are a capitalist invention (Fixed some basic stuff and resulted in a seg fault)

--- src/game/game_main.cpp
+++ src/game/game_main.cpp
@@ -13,18 +13,24 @@ void entity_registration() {}
 void setup() {
     player1 = phase_one_player();
     Renderer::turn_on_camera(0);
+
+    using namespace Input;
+    add(K(a), Name::LEFT);
+    add(K(d), Name::RIGHT);
+    add(K(w), Name::UP);
+    add(K(s), Name::DOWN);
 }
 
 // Main logic
 void update(f32 delta) {
-    integrate(player.player_body, delta);
+    player1.update(delta);
 }
 
 // Main draw
 void draw() {
     const char *some_string = "SeaCow";
     Renderer::draw_text(some_string, 0, 0, 1.0, ASSET_MONACO_FONT, -0.5);
-    debug_draw_body(player1.)
+    Physics::debug_draw_body(player1.get_body());
 }
 
 }  // namespace Game
--- src/game/phase_one_player.cpp
+++ src/game/phase_one_player.cpp
@@ -1,54 +1,106 @@
 //Here is where the phase one hero is Implemented
-static float TOP_SPEED = 2.0;
-static float TOP_ACCELERATION = 0.5;
+f32 TOP_SPEED = 1.0;
+f32 TOP_ACCELERATION = 0.5;
+f32 ACCELERATION_STEPS = 0.4;
+
 
 #include <vector>
 
-class phase_one_player{
+class phase_one_player {
 public:
-    phase_one_player();
-    void init_player();
-    void draw();
-    void move();
-    void accelerate (Vec2 new_vector);
 
+    phase_one_player();
+    void update(f32 delta);
     Physics::Body *get_body();
     
 private:
+
+    void jerk (Vec2 *new_vector);
+    void draw();
+    void move();
+    void accelerate();
+
+    f32 cos_acc_mod(Vec2 *currSpeed);
     
     bool space_movement = true;
-    Vec2 ammount_of_space_mov = (1.1, 1.1);
     Physics::Body *player_body;
+    
 
 // Vec2i current_pos = (0,0);
 // Vec2 acceleration_target = (0,0);
 // Vec2 current_speed = (0,0);    
 };
 
-phase_one_player(){
-    Vec2 points = { V2(0, 0), V2(1, 0), V2(1, 1), V2(1, 0) };
-    ShapeID my_shape = add_shape(LEN(points), points);
-    *player_body = *create_body(my_shape, 0b110); 
+
+//Constuct.
+phase_one_player::phase_one_player(){
+    Vec2 points[] = { V2(0, 0), V2(1, 0), V2(1, 1), V2(1, 0) };
+    Physics::ShapeID my_shape = Physics::add_shape(LEN(points), points);
+    *player_body = Physics::create_body(my_shape, 0b110); 
+
 }
 
-void init_player(){
+//Update
+void phase_one_player::update(f32 delta){
+
+    static bool show_control_controls = true;
+    if (Util::begin_tweak_section("phase1_controls", &show_control_controls)) {
+        Util::tweak("TOP_SPEED", &TOP_SPEED);
+        Util::tweak("TOP_ACCELERATION", &TOP_ACCELERATION);
+	Util::tweak("ACCELERATION_STEPS", &ACCELERATION_STEPS);
+    }
+    Util::end_tweak_section(&show_control_controls);
+
+    using namespace Input;
+    Vec2 vel_target = V2(0, 0);
+    if (down(Name::UP))
+        vel_target.y += ACCELERATION_STEPS;
+    if (down(Name::DOWN))
+        vel_target.y -= ACCELERATION_STEPS;
+    if (down(Name::LEFT))
+        vel_target.x -= ACCELERATION_STEPS;
+    if (down(Name::RIGHT))
+        vel_target.x += ACCELERATION_STEPS;
+    
+    jerk(&vel_target);
+
+}
+
+//Getters/Setters
+Physics::Body* phase_one_player::get_body(){
+    return player_body;
+}
+
+
+//Increase the current acceleration
+void phase_one_player::jerk(Vec2 *new_acc){
+    Vec2 acc_temp = player_body->acceleration + *new_acc;
+    
+    acc_temp.x = CLAMP(0, TOP_ACCELERATION, acc_temp.x);
+    acc_temp.y = CLAMP(0, TOP_ACCELERATION, acc_temp.y);
+
+    player_body->acceleration = acc_temp;
 }
 
 //Draw the Hero
-void draw(){
-    Renderer::push_rectangle(V2(2,2), V2(2,2), V4(1,0,0,1));
+void phase_one_player::draw(){
+    Renderer::push_rectangle(0,player_body->position, V2(2,2), V4(1,0,0,1));
 }
 
-void accelerate(Vec2 *new_acc){
-    Vec2 accc_temp += player_body->acceleration + new_acc;
+//Calc and Change the current speed by the acc and cos(max_speed - curr_speed)
+void phase_one_player::accelerate(){    
+
+}
+
+//Move by the curr speed
+void phase_one_player::move(){
     
-    acc_temp.x = CLAMP(0, TOP_ACCELERATION, accc_temp.x);
-    acc_temp.y = CLAMP(0, TOP_ACCELERATION, accc_temp.y);
-
-    player_body->acceleration = accc_temp;
 }
 
-Physics::Body *get_body(){
-    return player_body;
+//Calc the cos(of max speed - current speed)
+f32 phase_one_player::cos_acc_mod(Vec2 *currSpeed){
+    return 0.0;
 }
+
+
 
